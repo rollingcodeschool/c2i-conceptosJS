c3aac327fa3adc8f5223cde96a1587f35113fb5c
Emilse Arias <emilsearias@gmail.com>
2022-05-26 10:24:24 -0300

    agregamos, eliminamos y modificamos elementos en el arreglo de series

--- js/5-arrays.js
+++ js/5-arrays.js
@@ -11,4 +11,42 @@ console.log(series.length);
 // bucle para mostrar todos los elementos del arreglo
 for(let posicion=0; posicion < series.length; posicion++){
     document.write('<br>Elemento: '+series[posicion]);
-}
+};
+
+// cuando accedemos a elementos que no existen en el arreglo
+document.write('<br>'+series[10]);
+
+// agregar un elemento al final del arreglo
+series.push('Ozark');
+
+// bucle para mostrar todos los elementos del arreglo
+for(let posicion=0; posicion < series.length; posicion++){
+    document.write('<br>Elemento: '+series[posicion]);
+};
+
+// agregar un elemento en una posicion particular
+series.splice(3,0,'Breaking Bad');
+
+document.write('<hr>')
+for(let posicion=0; posicion < series.length; posicion++){
+    document.write('<br>Elemento: '+series[posicion]);
+};
+
+
+// eliminar elementos desde una posicion particular
+// series.splice(5,2); //(posicion del elemento, cantidad de elementos a eliminar);
+series.splice(7); //desde la posicion 5 borro todos los items a la derecha
+
+document.write('<hr>')
+for(let posicion=0; posicion < series.length; posicion++){
+    document.write('<br>Elemento: '+series[posicion]);
+};
+
+// modificar un elemento del arreglo
+series[6] = true;
+
+document.write('<hr>')
+for(let posicion=0; posicion < series.length; posicion++){
+    document.write('<br>Elemento: '+series[posicion]);
+};
+
