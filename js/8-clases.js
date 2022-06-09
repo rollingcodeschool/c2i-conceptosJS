098c2b02536a8b6f69746fded021eaa00313a039
Emilse Arias <emilsearias@gmail.com>
2022-06-09 11:04:02 -0300

    agregamos teoria de clases

--- js/8-clases.js
+++ js/8-clases.js
@@ -1,6 +1,7 @@
 
 // comision, antiguedad, horarios: (part-time, full-time), tipo de contrato 
 // crear una clase
+
 class Usuario{
     //primero creamos un metodo constructor
     constructor(nombreUsuarioParam, nombreParam, apellidoParam, correoParam, passwordParam, direccionParam){
@@ -45,13 +46,56 @@ class Usuario{
 // comision, notas, legajo, estado, asistencia
 class Alumno extends Usuario {
     //primer metodo
-    constructor(comision, legajo, estado, nombre, apellido, nombreUsuario, password, email, direccion ) {
+    constructor(comision, legajo, estado, nombre, apellido, nombreUsuario, password, email, direccion = 'sin direccion' ) {
     //    invocar al constructor de la clase usuario
         super(nombreUsuario, nombre, apellido, email, password, direccion);
         this.comision= comision;
         this.legajo = legajo;
-        this.estado = estado
+        this.estado = estado; // true: activo, false: suspendido
+        this.asistencia = 0; //propiedad por defecto
+        this.notas=[];
     } 
+
+    mostrarAlumno(){
+        document.write(`<ul>
+            <li>Comision: ${this.comision}</li>
+            <li>Legajo: ${this.legajo}</li>
+            <li>Estado: ${this.estado}</li>
+            <li>Asistencia: ${this.asistencia}</li>
+            <li>Notas: ${this.notas}</li>
+        </ul>`);
+    }
+
+    mostrarDatos(){
+        document.write(`<br>
+        <ul>
+            <li>Nombre: ${this.nombre}</li> 
+            <li>Apellido: ${this.apellido} </li>
+            <li>Correo: ${this.correo} </li>
+            <li>Direccion: ${this.direccion} </li>
+            <li>Nombre usuario: ${this.nombreUsuario} </li>
+            <li>Comision: ${this.comision}</li>
+            <li>Legajo: ${this.legajo}</li>
+            <li>Estado: ${this.estado}</li>
+            <li>Asistencia: ${this.asistencia}</li>
+            <li>Notas: ${this.notas}</li>
+        </ul>`)
+    }
+
+    deshabilitar(){
+        this.estado = false;
+    }
+    habilitar(){
+        this.estado = true;
+    }
+    // get y set
+
+    set modificarEstado(nuevoEstado){
+        this.estado = nuevoEstado;
+    }
+    get mostrarEstado(){
+        return this.estado;
+    }
 }
 
 // Como crear un objeto o instancia
@@ -69,3 +113,15 @@ document.write(`<br>La direccion del usuario ${userJuan.mostrarNombreUsuario} es
 userJuan.modificarDireccion = 'Tucumán'
 document.write(`<br>La direccion del usuario ${userJuan.mostrarNombreUsuario} es ${userJuan.mostrarDireccion}`);
 
+let alumnoMauro = new Alumno('2i',1233,true, 'Mauro', 'Garcia', 'mgarcia','3478kjdfhs','mauro@gmail.com','alguna direccion dgdfgdfg');
+let alumnoDaniela = new Alumno('2i',1233,true, 'Daniela', 'Cardozo', 'mgarcia','3478kjdfhs','mauro@gmail.com','tucuman');
+let alumnoHector = new Alumno('2i',1233,true, 'Hector', 'Norry', 'mgarcia','3478kjdfhs','mauro@gmail.com','tucuman');
+
+alumnoMauro.mostrarDatos();
+alumnoMauro.mostrarAlumno();
+console.log(alumnoMauro)
+
+console.log(alumnoMauro.mostrarDireccion);
+
+alumnoMauro.deshabilitar()
+console.log(alumnoMauro.mostrarEstado);
